075386ec2b02f9d64c869f71a593a35edafe2e27
Mae Pena <93175349+maepena91@users.noreply.github.com>
2022-07-02 11:50:22 -0700

    Update __init__.py

--- ballpy/__init__.py
+++ ballpy/__init__.py
@@ -1,12 +1,15 @@
 # config                    
 from flask import Flask, redirect
 from flask_migrate import Migrate
+from flask import Blueprint
+
+bp = Blueprint('reptile', __name__, url_prefix='/reptiles')
 
 # factory
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/ballpy'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ejueju1992!@localhost:5432/ballpy'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from . import models 
